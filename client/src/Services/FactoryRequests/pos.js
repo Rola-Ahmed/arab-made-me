ce1486855d0d85722d3d1c14067a204c050032ef
Rola-Ahmed <rolawafi209@gmail.com>
2024-07-21 20:00:05 +0300

    pos

---
+++ client/src/Services/FactoryRequests/pos.js
@@ -0,0 +1,22 @@
+import { getRequestDataHeader, putRequest } from "../authServices";
+
+export const getPrivateLables = async (param, header) => {
+  return await getRequestDataHeader(
+    `/factories/factory/privateLabelings?${param}`,
+    header,
+    {}
+  );
+};
+
+// export const updatePrivateLabel= async (id, header, data) => {
+//   return await putRequest(`/privateLabelings/factory/${id}`, header, data);
+// };
+
+
+// export const getOnePrivateLable = async (param, header, data) => {
+//   return await getRequestDataHeader(`/rfqs?${param}`, header, data);
+// };
+
+// export const updateRFQ = async (id, header, data) => {
+//   return await putRequest(`/rfqs/factory/${id}`, header, data);
+// };
