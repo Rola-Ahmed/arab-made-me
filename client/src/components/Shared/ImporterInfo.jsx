773899a73987a3dbb8fc0cf47d296d7d5a450e7f
Rola-Ahmed <rolawafi209@gmail.com>
2024-07-23 10:56:14 +0300

    read only

--- client/src/components/Shared/ImporterInfo.jsx
+++ client/src/components/Shared/ImporterInfo.jsx
@@ -1,3 +1,4 @@
+import ReadOnly from "components/Forms/Shared/ReadOnly";
 export default function ImporterInfo(props) {
   let { importerData } = props;
   return (
@@ -9,49 +10,28 @@ export default function ImporterInfo(props) {
         <div className="w-100 ">
           <div className="row  row-gap">
             <div className="col-6">
-              <div className="grid-gap-col">
-                <div className="form-group">
-                  <label>Representative Name</label>
-                  <input
-                    type="text"
-                    className="form-control text-dark"
-                    value={`${
-                      importerData?.repName == null
-                        ? " "
-                        : `${importerData?.repName}`
-                    }`}
-                    readOnly
-                  />
-                </div>
-              </div>
+              <ReadOnly
+                title="Product Name"
+                value={`${
+                  importerData?.repName == null
+                    ? " "
+                    : `${importerData?.repName}`
+                }`}
+              />
             </div>
 
             <div className="col-6">
-              <div className="grid-gap-col">
-                <div className="form-group">
-                  <label> Representative email</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    value={importerData?.repEmail || ""}
-                    readOnly
-                  />
-                </div>
-              </div>
+              <ReadOnly
+                title=" Representative email"
+                value={importerData?.repEmail}
+              />
             </div>
 
             <div className="col-6">
-              <div className="grid-gap-col">
-                <div className="form-group">
-                  <label>Representative phone number</label>
-                  <input
-                    type="text"
-                    className="form-control"
-                    value={importerData?.repPhone || ""}
-                    readOnly
-                  />
-                </div>
-              </div>
+              <ReadOnly
+                title="Representative phone number"
+                value={importerData?.repPhone}
+              />
             </div>
           </div>
         </div>
