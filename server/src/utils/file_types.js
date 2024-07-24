4246d3f83b91d624966921c39689396b1172d9cd
Rola-Ahmed <rolawafi209@gmail.com>
2024-07-24 20:26:14 +0300

    removed extra files

--- server/src/utils/file_types.js
+++
@@ -1,9 +0,0 @@
-
-let imageTypes=['image/jpeg', 'image/jpg', 'image/png', 'image/gif']
-let fileTypes=[...imageTypes,'application/pdf','application/docx']
-let videoTypes=['video/mkv','video/x-matroska','video/mp4', 'video/webm']
-export const filesTypes={
-    image:imageTypes,
-    file:fileTypes,
-    video:videoTypes
-}
