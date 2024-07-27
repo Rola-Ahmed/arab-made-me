e3e92cf779adbcd294fee634fbec53f45fa16598
Rola-Ahmed <rolawafi209@gmail.com>
2024-07-27 23:58:43 +0300

    cursor for offer

--- client/src/components/Sourcinghub/SourcingOffers/SourcingOfferCard.jsx
+++ client/src/components/Sourcinghub/SourcingOffers/SourcingOfferCard.jsx
@@ -1,11 +1,20 @@
 import { baseUrl_IMG } from "config.js";
+import { useNavigate } from "react-router-dom";
 import { handleImageError } from "utils/ImgNotFound";
 import { getMonthName as getDate } from "utils/getMonthName";
 
 export default function SourcingOfferCard({ item }) {
   let getMonthName = getDate;
+  let navigate = useNavigate();
   return (
-    <tr className="cursor">
+    <tr
+      className="cursor"
+      onClick={() => {
+        navigate(
+          `/sourcingOffer/${item?.id}?factoryOffersId=${item?.id}&productName=${item?.productName}&factoryId=${item?.factoryId}`
+        );
+      }}
+    >
       <th className="  " scope="row">
         <div className="d-flex flag-container align-items-center">
           <img
