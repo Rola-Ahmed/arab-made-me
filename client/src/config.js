744c8148f9d36fe0c58f31b872e97ef3d3139260
Rola-Ahmed <rolawafi209@gmail.com>
2024-08-01 15:29:53 +0300

    config

--- client/src/config.js
+++ client/src/config.js
@@ -1,18 +1,15 @@
 import io from "socket.io-client";
 // export const baseUrl = "http://localhost:5000/api/v1";
 
-export const baseUrl = "https://backend-rail-subbase-production.up.railway.app/api/v1";
-// process.env.REACT_APP_NODE_ENV == "production"
-// ?
-// process.env.REACT_APP_BASE_URL;
-// : process.env.REACT_APP_DEV_BASE_URL;
+export const baseUrl =
+  process.env.REACT_APP_NODE_ENV == "production"
+    ? process.env.REACT_APP_BASE_URL
+    : process.env.REACT_APP_DEV_BASE_URL;
 
-// export const baseUrl_IMG = "http://localhost:5000";
-export const baseUrl_IMG = "https://backend-rail-subbase-production.up.railway.app";
-// process.env.REACT_APP_NODE_ENV == "production"
-// ?
-// process.env.REACT_APP_IMAGE_URL;
-// : process.env.REACT_APP_DEV_IMAGE_URL;
+export const baseUrl_IMG =
+  process.env.REACT_APP_NODE_ENV == "production"
+    ? process.env.REACT_APP_IMAGE_URL
+    : process.env.REACT_APP_DEV_IMAGE_URL;
 
 export const socket = io.connect(baseUrl_IMG, {
   transports: ["websocket", "polling"],
