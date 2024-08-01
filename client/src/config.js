057756635ebb3383ad112d0f0695cea6b9ae82d5
Rola-Ahmed <rolawafi209@gmail.com>
2024-08-01 15:39:10 +0300

    config

--- client/src/config.js
+++ client/src/config.js
@@ -1,22 +1,26 @@
 import io from "socket.io-client";
 // export const baseUrl = "http://localhost:5000/api/v1";
 
+// export const baseUrl =
+//   process.env.REACT_APP_NODE_ENV == "production"
+//     ? process.env.REACT_APP_BASE_URL
+//     : process.env.REACT_APP_DEV_BASE_URL;
+
+// export const baseUrl_IMG =
+//   process.env.REACT_APP_NODE_ENV == "production"
+//     ? process.env.REACT_APP_IMAGE_URL
+//     : process.env.REACT_APP_DEV_IMAGE_URL;
+
 export const baseUrl =
-  process.env.REACT_APP_NODE_ENV == "production"
-    ? process.env.REACT_APP_BASE_URL
-    : process.env.REACT_APP_DEV_BASE_URL;
+  "https://backend-rail-subbase-production.up.railway.app/api/v1";
 
 export const baseUrl_IMG =
-  process.env.REACT_APP_NODE_ENV == "production"
-    ? process.env.REACT_APP_IMAGE_URL
-    : process.env.REACT_APP_DEV_IMAGE_URL;
+  "https://backend-rail-subbase-production.up.railway.app";
 
 export const socket = io.connect(baseUrl_IMG, {
   transports: ["websocket", "polling"],
   autoConnect: true,
 });
-
-
 
 // export const socket = io(baseUrl_IMG, {
 //   autoConnect: true, // Do not connect automatically
