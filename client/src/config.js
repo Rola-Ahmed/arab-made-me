8ab7d54a2fe5d8b7369cbdf96369b93ebda0aa5f
Rola-Ahmed <rolawafi209@gmail.com>
2024-08-01 15:32:14 +0300

    comments

--- client/src/config.js
+++ client/src/config.js
@@ -16,10 +16,7 @@ export const socket = io.connect(baseUrl_IMG, {
   autoConnect: true,
 });
 
-// export const baseUrl = "http://13.60.166.80:5000/api/v1";
-// // : process.env.REACT_APP_DEV_BASE_URL;
 
-// export const baseUrl_IMG = "http://13.60.166.80:5000";
 
 // export const socket = io(baseUrl_IMG, {
 //   autoConnect: true, // Do not connect automatically
