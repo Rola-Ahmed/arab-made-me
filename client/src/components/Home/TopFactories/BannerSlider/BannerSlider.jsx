db0d32863e55e9fa4f3e2897effeae81a7c9cc90
Rola-Ahmed <rolawafi209@gmail.com>
2024-08-03 17:02:14 +0300

    swiper instead of reactslick

--- client/src/components/Home/TopFactories/BannerSlider/BannerSlider.jsx
+++ client/src/components/Home/TopFactories/BannerSlider/BannerSlider.jsx
@@ -1,49 +1,25 @@
-import CustomSlider from "react-slick";
 import { handleImageError } from "utils/ImgNotFound";
 import { baseUrl_IMG } from "config.js";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import { Pagination } from "swiper/modules";
 
 export default function BannerSlider(props) {
   let { factoryitem } = props;
-  const settings = {
-    dots: true,
-    fade: true,
-    arrows: false,
-    appendDots: (dots) => (
-      <div
-        style={{
-          height: "1rem",
-          bottom: "1.5rem",
-          display: "flex",
-          justifyContent: "center",
-          position: "absolute",
-          top: "10rem",
-          zIndex: 1,
-        }}
-      >
-        {dots}
-      </div>
-    ),
-
-    focusOnSelect: true,
-  };
   return (
-    <CustomSlider {...settings}>
+    <Swiper pagination={{ clickable: true }} modules={[Pagination]}>
       {factoryitem?.images?.map((item, index) => (
-        <img
-          key={item?.id}
-          src={`${baseUrl_IMG}/${item}`}
-          className="sliderImg"
-          alt={`slide ${item} `}
-          onError={handleImageError}
-        />
+        <SwiperSlide>
+          <img
+            key={item?.id}
+            src={`${baseUrl_IMG}/${item}`}
+            className="sliderImg w-100"
+            alt={`slide ${item} `}
+            onError={handleImageError}
+          />
+        </SwiperSlide>
       ))}
-
-      <img
-        src={`${baseUrl_IMG}/${factoryitem?.coverImage}`}
-        className="sliderImg"
-        alt={`slide coverImage `}
-        onError={handleImageError}
-      />
-    </CustomSlider>
+    </Swiper>
   );
 }
