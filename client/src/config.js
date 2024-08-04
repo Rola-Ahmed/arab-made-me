3a58cc0345c05f372e9104c5cdaf501dbc3095f0
Rola-Ahmed <rolawafi209@gmail.com>
2024-08-04 09:11:01 +0300

    config

--- client/src/config.js
+++ client/src/config.js
@@ -1,29 +1,17 @@
 import io from "socket.io-client";
-// export const baseUrl = "http://localhost:5000/api/v1";
 
-export const baseUrl = "https://backend-rail-subbase-production.up.railway.app/api/v1";
-// process.env.REACT_APP_NODE_ENV == "production"
-// ?
-// process.env.REACT_APP_BASE_URL;
-// : process.env.REACT_APP_DEV_BASE_URL;
+export const baseUrl =
+  process.env.REACT_APP_NODE_ENV == "production"
+    ? process.env.REACT_APP_BASE_URL
+    : process.env.REACT_APP_DEV_BASE_URL;
 
-// export const baseUrl_IMG = "http://localhost:5000";
-export const baseUrl_IMG = "https://backend-rail-subbase-production.up.railway.app";
-// process.env.REACT_APP_NODE_ENV == "production"
-// ?
-// process.env.REACT_APP_IMAGE_URL;
-// : process.env.REACT_APP_DEV_IMAGE_URL;
+export const baseUrl_IMG =
+  process.env.REACT_APP_NODE_ENV == "production"
+    ? process.env.REACT_APP_IMAGE_URL
+    : process.env.REACT_APP_DEV_IMAGE_URL;
+
 
 export const socket = io.connect(baseUrl_IMG, {
   transports: ["websocket", "polling"],
   autoConnect: true,
 });
-
-// export const baseUrl = "http://13.60.166.80:5000/api/v1";
-// // : process.env.REACT_APP_DEV_BASE_URL;
-
-// export const baseUrl_IMG = "http://13.60.166.80:5000";
-
-// export const socket = io(baseUrl_IMG, {
-//   autoConnect: true, // Do not connect automatically
-// });
