87716c9cce58984954d46865554ef81b998d738d
Rola-Ahmed <rolawafi209@gmail.com>
2024-08-11 00:59:35 +0300

    place

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,16 +1,18 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import "./pagination.css";
 
-import App from "./App";
+
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import "assets/css/all.min.css";
 
+import "assets/css/pagination.css";
+import "assets/css/all.min.css";
 import { UserTokenProvider } from "./Context/userToken";
 import { UserTypeProvider } from "./Context/userType";
 import { GlobalMessage } from "./Context/globalMessage";
 import "./i18n";
+
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
