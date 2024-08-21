32a7bcf8f7d4713f9e8f655a0b9825431d5506a7
Rola-Ahmed <rolawafi209@gmail.com>
2024-08-22 01:00:41 +0300

    chanefs

--- client/src/roles/factorydashRoles.js
+++ client/src/roles/factorydashRoles.js
@@ -1,13 +1,7 @@
 // src/roles/factorydashRoles.js
 import { Navigate } from "react-router-dom";
-import ErrorToast from "../components/ErrorToast"; // Adjusted import path
+import ErrorToast from "components/ErrorToast"; // Adjusted import path
 
-// Function to determine the appropriate navigation path based on user data
-const getNavigationPath = (userRole, importerId) => {
-  if (importerId) return "/importerdashboard/403?refresh";
-  if (userRole === "admin") return "/adminDashboard/403?refresh";
-  return "/403";
-};
 
 // Authorization check function
 export const checkFactorydashAuthorization = (isLogin, currentUserData) => {
@@ -21,22 +15,22 @@ export const checkFactorydashAuthorization = (isLogin, currentUserData) => {
 
   // If the user is logged in, check user data
   if (currentUserData) {
-    const { factoryId, importerId, userRole } = currentUserData;
+    const { factoryId, userRole } = currentUserData;
 
     // Log user data for debugging purposes
-    console.log("User Data:", { factoryId, importerId, userRole });
+    console.log("User Data:", { factoryId, userRole });
 
     if (!factoryId) {
-      return;
-    } else if (importerId) {
-      ErrorToast("You are not authorized to access");
-      return <Navigate to="/importerdashboard/403?refresh" />;
-    } else if (userRole == "admin") {
-      ErrorToast("You are not authorized to access");
-      return <Navigate to="/adminDashboard/403?refresh" />;
-    } else if (userRole == "user") {
-      ErrorToast("You are not authorized to access");
-      return <Navigate to="/403" />;
+      if (userRole == "importer") {
+        ErrorToast("You are not authorized to access");
+        return <Navigate to="/importerdashboard/403?refresh" />;
+      } else if (userRole == "admin") {
+        ErrorToast("You are not authorized to access");
+        return <Navigate to="/adminDashboard/403?refresh" />;
+      } else if (userRole == "user") {
+        ErrorToast("You are not authorized to access");
+        return <Navigate to="/403" />;
+      }
     }
   }
 
