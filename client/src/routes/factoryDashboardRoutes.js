b6f01047ceb528ac977d738c6f1e5ac24d5df958
Rola-Ahmed <rolawafi209@gmail.com>
2024-08-21 23:10:00 +0300

    path changes

--- client/src/routes/factoryDashboardRoutes.js
+++ client/src/routes/factoryDashboardRoutes.js
@@ -1,5 +1,5 @@
 // Factory Dashboard pages
-import Factorydash from "containers/Factorydashboard/FactorydashContainer";
+import Factorydash from "components/Factorydashboard/FactorydashContainer";
 
 // private Label Data
 import PrivateLabeLReq from "components/Factorydashboard/subComponets/PrivateLabelRequest/Index/IndexContainer";
