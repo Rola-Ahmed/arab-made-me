977b67f6097cc83d9107699d247f63fea6977f2a
Rola-Ahmed <rolawafi209@gmail.com>
2024-08-26 14:20:10 +0300

    coomments

--- client/src/components/Home/SourcingHub/SourcingOffers/SourcingOffers.jsx
+++ client/src/components/Home/SourcingHub/SourcingOffers/SourcingOffers.jsx
@@ -6,7 +6,6 @@ import { useAppTranslation } from "config.js";
 export default function SourcingOffers({ allSourcingOffer, apiStatus }) {
   const { trans: t, currentLang } = useAppTranslation();
 
-
   return (
     <>
       {/* home page */}
@@ -25,6 +24,7 @@ export default function SourcingOffers({ allSourcingOffer, apiStatus }) {
           </div>
         </div>
 
+        {/* loading or error case */}
         {apiStatus?.laoding && (
           <>
             <div className="position-absolute error-float">
@@ -41,7 +41,8 @@ export default function SourcingOffers({ allSourcingOffer, apiStatus }) {
           </>
         )}
 
-        {/* error */}
+        {/* sucess state but data is empty */}
+
         {allSourcingOffer?.length == 0 && !apiStatus?.laoding && (
           <div className="position-absolute error-float">
             <p className="fs-5 text-muted fw-bolder text-5 mt-5 pt-5 mx-auto">
