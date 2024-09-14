3b07b216248f273715f8707fe631f57b1585d883
Rola-Ahmed <rolawafi209@gmail.com>
2024-09-14 23:02:34 +0300

    seerver

--- server/index.js
+++ server/index.js
@@ -10,48 +10,31 @@ import express from "express";
 import { bootstrap } from "./src/index.router.js";
 // import { Server } from "socket.io"
 import { initIo, socketAuth } from "./src/utils/socket_server.js";
-import { Server } from "socket.io";
+// import { Server } from "socket.io";
 const app = express();
 var whitelist = ["arab-made.com"];
-var imgAllow = "https://arab-made.com";
-app.use(helmet())
-app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: ["'self'"],
-      imgSrc: ["'self'", "data:", imgAllow], // Correct usage
-    },
-  })
-);
+// var imgAllow = "https://arab-made.com";
+// app.use(helmet())
+// app.use(
+//   helmet.contentSecurityPolicy({
+//     directives: {
+//       defaultSrc: ["'self'"],
+//       imgSrc: ["'self'", "data:", imgAllow], // Correct usage
+//     },
+//   })
+// );
 
 app.use("/uploads", express.static("uploads"));
 app.use(express.json());
 
-// var corsOptions = {
-//     origin: function (origin, callback) {
-//         if (process.env.MODE == 'DEV') {
-//             callback(null, true)
-//         }
-//         else if (!origin) {
-//             if (this.req && this.req.headers.password == process.env.PASSWORD_API) {
-//                 callback(null, true)
-//             } else {
-//                 callback(new Error('Not allowed by CORS, Wrong password'))
-//             }
-//         } else if (whitelist.includes(new URL(origin).hostname)) {
-//             callback(null, true)
-//         } else {
-//             callback(new Error('Not allowed by CORS'))
-//         }
-//     }
-// }
+
 
 app.use(cors());
 
 app.use((req, res, nxt) => {
   if (process.env.MODE == "DEV") nxt();
   else if (whitelist.includes(req.hostname)) nxt();
-  else if (req.headers["api-key"] == process.env.PASSWORD_API) nxt();
+  // else if (req.headers["api-key"] == process.env.PASSWORD_API) nxt();
   else return res.status(403).json({ message: "error" });
 });
 
