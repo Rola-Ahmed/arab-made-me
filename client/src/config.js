98ad7844b36b41605a3078b8a69ab50aff4bdc61
Rola-Ahmed <rolawafi209@gmail.com>
2024-09-21 17:10:18 +0300

    config

--- client/src/config.js
+++ client/src/config.js
@@ -1,19 +1,8 @@
 import io from "socket.io-client";
 import { useTranslation } from "react-i18next";
 
-
 export const baseUrl ='https://arab-made.com/api/v1'
-  // process.env.REACT_APP_NODE_ENV == "production"
-  //   ? process.env.REACT_APP_BASE_URL
-  //   : process.env.REACT_APP_DEV_BASE_URL;
-
 export const baseUrl_IMG ='https://arab-made.com'
-  // process.env.REACT_APP_NODE_ENV == "production"
-  //   ? process.env.REACT_APP_IMAGE_URL
-  //   : process.env.REACT_APP_DEV_IMAGE_URL;
-
-
-export const map_api_Key = "gqEosgdSKdu6kLxXYi6ubsnOdssVsZL4EV8K60I7Xr4";
 
 
 export const socket = io.connect(baseUrl_IMG, {
@@ -24,16 +13,23 @@ export const socket = io.connect(baseUrl_IMG, {
   },
 });
 
+// export const useAppTranslation = () => {
+//   const { t: trans, i18n } = useTranslation(["forms", "translation","BtnsDescription"]);
+//   const currentLang = i18n.language;
+//   return { trans, currentLang };
+// };
 
-// const { t: trans, i18n } = useTranslation();
-// const currentLang = i18n.language; // 'languages' gives an array; use 'language' for the current language
-
-// export { trans, currentLang };
 
 
 // Custom function to use translation hook
 export const useAppTranslation = () => {
-  const { t: trans, i18n } = useTranslation(["forms", "translation","BtnsDescription"]);
+  const { t: trans, i18n } = useTranslation([
+    "forms",
+    "translation",
+    "BtnsDescription",
+    "countries",
+    "sectors",
+  ]);
   const currentLang = i18n.language;
   return { trans, currentLang };
 };
