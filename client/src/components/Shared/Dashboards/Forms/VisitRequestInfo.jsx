2653704bbf653a591bf7e07b90f41a6ab806e8dd
Rola-Ahmed <rolawafi209@gmail.com>
2024-10-08 13:39:36 +0300

    changes

--- client/src/components/Shared/Dashboards/Forms/VisitRequestInfo.jsx
+++ client/src/components/Shared/Dashboards/Forms/VisitRequestInfo.jsx
@@ -12,29 +12,33 @@ export default function VisitRequestInfo({ requestedData }) {
         <p> Factory Visit Details</p>
         <div className="w-100 ">
           <div className="row  row-gap">
-
-          {
-              [
-                { title: "Visit Purpose", value: requestedData?.purpose },
-                {  title="requested Visit Date", value:`${formattedTime(requestedData?.date)}` },
-                { title: "Visit Type", value: requestedData?.visitType, },
-                { title: "Individuals Number", value: requestedData?.individualsNumber },
-                { title: "Required Products", value: requestedData?.requiredProducts },
-                { title: "status", value: requestedData?.status },
-                { title: "Created At", value: `${getMonthName(requestedData?.createdAt?.split("T")?.[0])}` },
-               ]?.map(({ title, value }, index) => (
-                <div className="col-md-6 col-sm-12" key={index}>
-                  <ReadOnly title={title} value={value} />
-                </div>
-              ))}
-
-
-           
-
-          
-
-           
-         
+            {[
+              { title: "Visit Purpose", value: requestedData?.purpose },
+              {
+                title: "requested Visit Date",
+                value: `${formattedTime(requestedData?.date)}`,
+              },
+              { title: "Visit Type", value: requestedData?.visitType },
+              {
+                title: "Individuals Number",
+                value: requestedData?.individualsNumber,
+              },
+              {
+                title: "Required Products",
+                value: requestedData?.requiredProducts,
+              },
+              { title: "status", value: requestedData?.status },
+              {
+                title: "Created At",
+                value: `${getMonthName(
+                  requestedData?.createdAt?.split("T")?.[0]
+                )}`,
+              },
+            ]?.map(({ title, value }, index) => (
+              <div className="col-md-6 col-sm-12" key={index}>
+                <ReadOnly title={title} value={value} />
+              </div>
+            ))}
           </div>
         </div>
       </div>
